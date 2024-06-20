6843377fc7223ce03a33fe1e2e99a79d76bcde9e
meklis <max.boyar.a@gmail.com>
2024-06-20 16:43:00 +0300

    Fix build for working in docker

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ cp ./configs/trap-listener.yml ./internal/config
 cp ./configs/trap-listener.yml ./build
 
 go mod download
-GOOS=linux GOARCH=amd64 go build -o ./build/traplistener ./cmd/listener/main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./build/traplistener ./cmd/listener/main.go
 rm -f ./internal/config/trap-listener.yml
 
 
